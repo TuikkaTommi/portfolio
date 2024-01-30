4eb1d1c22dc1b42bedde9cf210e0947dc4fadb15
TuikkaTommi <N5008@student.jamk.fi>
2024-01-30 14:51:24 +0200

    Update Player.js

--- Express/playerscorebackend/models/Player.js
+++ Express/playerscorebackend/models/Player.js
@@ -5,7 +5,6 @@ const PlayerSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, max: 20 },
   email: { type: String, required: true, unique: true, max: 80 },
   roundsplayed: { type: Number, required: false, min: 0 },
-  // lisää pelattuejn kierrosten määrä?
   scores: { type: [ScoreSchema], required: true },
 });
 
